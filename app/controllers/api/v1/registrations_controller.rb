eb508fb398863ff32e8dee891f66c398a318fafc
Alex Vlasov <drumlife182@gmail.com>
2022-04-04 13:54:50 +0200

    added json_response function

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -4,20 +4,9 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
   def create
     user = User.new user_params
     if user.save
-      render json: {
-        message: 'Signed Up Successfully',
-        is_success: true,
-        data: {
-          user: user
-        }
-      }, status: :ok
+      json_response('Signed Up successfully', true, { user: user }, :ok)
     else
-      # json_response('Something wrong', false, {}, :unprocessable_entity)
-      render json: {
-        message: 'Something wrong',
-        is_success: false,
-        data: {}
-      }, status: :unprocessable_entity
+      json_response('Something wrong', false, {}, :unprocessable_entity)
     end
   end
 
@@ -30,11 +19,6 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
   def ensure_params_exist
     return if params[:user].present?
 
-    # json_response('Missing params', false, {}, :bad_request)
-    render json: {
-      message: 'Missing params',
-      is_success: false,
-      data: {}
-    }, status: :bad_request
+    json_response('Missing params', false, {}, :bad_request)
   end
 end
