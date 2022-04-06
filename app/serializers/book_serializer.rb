099a24995fd2a77f3336abc8d48b384045a8e96a
Alex Vlasov <drumlife182@gmail.com>
2022-04-06 18:29:26 +0200

    added eager to project

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -1,9 +1,9 @@
 class BookSerializer < ActiveModel::Serializer
-  attributes :id, :title, :author, :image,
-             #  :average_rating_of_book, :content_rating_of_book, :recommend_rating_of_book,
-             :total_reviews
+  attributes :id, :title, :author, :image
+  #  :average_rating_of_book, :content_rating_of_book, :recommend_rating_of_book,
+  #  :total_reviews
 
-  # has_many :reviews
+  has_many :reviews
 
   # def average_rating_of_book
   #   object.reviews.count == 0 ? 0 : object.reviews.average(:average_rating)
@@ -17,7 +17,7 @@ class BookSerializer < ActiveModel::Serializer
   #   object.reviews.count == 0 ? 0 : object.reviews.average(:recommend_rating)
   # end
 
-  def total_reviews
-    object.reviews_count
-  end
+  # def total_reviews
+  #   object.reviews_count
+  # end
 end
